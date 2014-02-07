1f82eb7e0e8d3315958aea2249eb1c82645f03a6
Mario Michelli <mario@edison.no>
2014-02-07 10:50:41 +0100

    added videos

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+document.createElement('video');
+document.createElement('audio');
+
+
+
+$( document ).ready(function() {
+
+
+
+    $(window).resize(resize);
+
+
+    var is = setInterval(resize, 100);
+
+    function resize() {
+        var $v = $("#TopVideo video");
+        if($v){
+            var w = $v.width();
+            var h = $v.height();
+            $v.css("margin-left", w/2 * -1);
+            $v.css("margin-top", h/2 * -1);
+            if(is){
+                clearInterval(is);
+                $v.addClass("loaded");
+                is = false;
+                $v.get(0).volume = 0.1;
+            }
+
+        }
+    }
+
+
+
+
+});
